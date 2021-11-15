d5621b57191ce9ee83cb62a5fdad2a8545f5122f
Juan David <you@example.com>
2021-11-15 18:32:24 -0500

    Html,Css,JavaScript

---
+++ Web/codigo.js
@@ -0,0 +1,98 @@
+//saludar();
+//console.log(saludar());
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+const guessExit = document.querySelector(".exitSubmit");
+let guessCount = 1;
+let resetButton;
+
+function exitPage() {
+  // this funtion is a method to understand the confirm part of window, intelligent alignament
+  //  console.log(cierra);
+  if (confirm("¿Estas seguro que desea salir?")) {
+    window.open(" ", "_self").window.close();
+  }
+}
+
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses: ";
+  }
+
+  guesses.textContent += userGuess + " ";
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.style.backgroundColor = "green";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!!GAME OVER!!!";
+    lowOrHi.textContent = "";
+    setGameOver();
+  } else {
+    lastResult.textContent = "Wrong!";
+    lastResult.style.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Last guess was too high!";
+    }
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+guessExit.addEventListener("click", exitPage);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (let i = 0; i < resetParas.length; i++) {
+    resetParas[i].textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+  lastResult.style.backgroundColor = "white";
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+function cambioTitle() {
+  let nombre = prompt("Digita tu nombre");
+  if (nombre != null) {
+    document.title = nombre;
+  }
+}
+//cambioTitle();
+
+//document.write("Hola");
+//nombre = "Juan David";
+//frase = `Hola ${nombre}, como estas?`;
+
+//document.write(frase);
+
+function saludar() {
+  alert("This function work");
+}
